f94f4630140261a7919ce96c85e02b48feb57d5d
zangqilong <zql@liulishuo.com>
2015-06-08 09:07:52 +0800

    Initial Commit

---
+++ QQTransition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QQTransition
+//
+//  Created by 臧其龙 on 15/6/8.
+//  Copyright (c) 2015年 臧其龙. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
